d3ea658932ad714ba8e253240de02ccacca77089
MD. Muhibur Rahman Nabil <121649277+a2-nabil@users.noreply.github.com>
2025-02-04 15:11:04 +0600

    Updated ImageUploader.tsx

--- components/ImageUploader.tsx
+++ components/ImageUploader.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import Image from 'next/image'
 
 export default function ImageUploader({ onUpload }: { onUpload: (file: File) => void }) {
   const [preview, setPreview] = useState<string | null>(null);
@@ -19,7 +20,7 @@ export default function ImageUploader({ onUpload }: { onUpload: (file: File) => 
       <label htmlFor="fileInput" className="cursor-pointer px-4 py-2 bg-blue-500 text-white rounded-md">
         Upload Image
       </label>
-      {preview && <img src={preview} alt="Preview" className="mt-4 w-64 h-64 object-cover rounded-lg" />}
+      {preview && <Image src={preview} alt="Preview" className="mt-4 w-64 h-64 object-cover rounded-lg" />}
     </div>
   );
 }
